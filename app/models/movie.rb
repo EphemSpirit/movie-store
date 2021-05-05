c4fec761ead7f5bcfceb499700c55e714a429bdc
Drew Hund <languageinvestigator@gmail.com>
2021-05-05 17:06:42 -0400

    made associations work

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ApplicationRecord
   validates :title, :description, :rating, :genre, :release_date, presence: true
+
+  has_one :director
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_204751) do
+ActiveRecord::Schema.define(version: 2021_05_05_210325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "directors", force: :cascade do |t|
+    t.string "name"
+    t.integer "years_active"
+    t.integer "movie_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -23,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_05_05_204751) do
     t.date "release_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "director_id"
   end
 
 end
