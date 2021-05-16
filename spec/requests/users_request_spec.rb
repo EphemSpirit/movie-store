ad15ee2a24b7cd82da5cc2d3b0383f598bcd2290
Drew Hund <languageinvestigator@gmail.com>
2021-05-16 14:55:53 -0400

    finished user tests

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -4,9 +4,26 @@ RSpec.describe "Users", type: :request do
 
   let!(:user) { create(:user) }
 
-  describe "basic func" do
-    it "lets me use the helper method" do
+  context "not signed in" do
+    it "redirects a non-logged in user" do
+      get "/api/v1/user/#{user.id}"
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  it "gets the index page" do
+    get "/api/v1/users"
+    expect(response).to have_http_status(200)
+    expect(JSON.parse(response.body).length).to eq(1)
+  end
+
+  context "signed in" do
+    it "gets a user page when signed in" do
+      get "/api/v1/user/#{user.id}"
+      expect(response).to have_http_status(302)
       sign_in user
+      get "/api/v1/user/#{user.id}"
+      expect(response).to have_http_status(200)
     end
   end
 end
